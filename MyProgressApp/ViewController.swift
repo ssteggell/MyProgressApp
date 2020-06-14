9c2cb43ddf521bdac857cc160303d7b88565cd53
Spencer Steggell <spencer.steggell@gmail.com>
2020-06-14 14:24:36 -0700

    Initial Commit

---
+++ MyProgressApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProgressApp
+//
+//  Created by Spencer Steggell on 6/14/20.
+//  Copyright © 2020 Spencer Steggell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
